83339072874d8639a0149a803d6b084c5c565de5
Emmanuel <67012829+Plaza99@users.noreply.github.com>
2023-06-29 17:40:09 +0200

    Deleted java app directory

--- SmartWinemaker/src/main/unipi/iot/model/Topic.java
+++
@@ -1,6 +0,0 @@
-package main.unipi.iot.model;
-
-public interface Topic{
-	public Long getNodeId();
-	public int getValue();
-}
--- SmartWinemaker/src/main/unipi/iot/mqtt/devices/TemperatureHandler.java
+++
@@ -1,8 +0,0 @@
-package main.unipi.iot.mqtt.devices;
-
-import main.unipi.iot.mqtt.MqttHandler;
-
-public class TemperatureHandler extends MqttHandler{
-
-		
-}
