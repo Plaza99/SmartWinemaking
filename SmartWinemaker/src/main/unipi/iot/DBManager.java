506908593b11f1c41b90b5d0461619c46730ecb1
Fochi1999 <73483730+Fochi1999@users.noreply.github.com>
2023-07-02 18:12:52 +0100

    DBManager new operations

--- SmartWinemaker/src/main/unipi/iot/DBManager.java
+++ SmartWinemaker/src/main/unipi/iot/DBManager.java
@@ -1,19 +1,32 @@
 package main.unipi.iot;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import main.unipi.iot.mqtt.devices.messages.TemperatureMessage;
+
 public class DBManager {
 //TODO da fare
 
-    private static String MySqlIp;
-    private static int MySqlPort;
-    private static String MySqlUsername;
-    private static String MySqlPassword;
-    private static String MySqlName;
-
+    private static String MySqlIp="127.0.0.1";
+    private static int MySqlPort=3306;
+    private static String MySqlUsername="root";
+    private static String MySqlPassword="root";
+    private static String MySqlDbName="winemaking";
+    private static DBManager instance;
     //TODO set password,ip,name,username and port
+    public static DBManager getInstance() {
+        if(instance == null)
+            instance = new DBManager();
+
+        return instance;
+    }
 
     private Connection getConnection() throws SQLException {
             return DriverManager.getConnection("jdbc:mysql://"+ MySqlIp + ":" + MySqlPort +
-                            "/" + MySqlName + "?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=CET",
+                            "/" + MySqlDbName + "?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=CET",
                     MySqlUsername, MySqlPassword);
         }
 
@@ -47,6 +60,26 @@ public class DBManager {
             {
                 e.printStackTrace();
             }
+    }
+    public void insertNewActuator(String ip,String type) {
+    	try (
+                Connection connection = getConnection();
+                PreparedStatement statement = connection.prepareStatement(
+                        "REPLACE INTO `actuator`(`ip`, `type`) VALUES (?, ?)");
+        )
+        {
+            statement.setString(1, ip);
+            statement.setString(2, type);
+            statement.executeUpdate();
+        }
+        catch (final SQLException e) {
+        	e.printStackTrace();
+        }
+    }
+    public void insertSampleTemperature(TemperatureMessage m) {
+    	String[] attr= {"id","temperature"};
+    	String[] values= { Long.toString(m.node),Integer.toString(m.temperature)};
+    	this.insert("temperature", 2, attr, values);
     }
     
 }
